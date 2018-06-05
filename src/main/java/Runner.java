f6a56b0062f15f48a1c1756d4f384131bb832e8b
pac9 <pac9@btinternet.com>
2018-06-05 23:06:09 +0100

    game runs until out of money or player exits

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,13 @@
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
+import java.util.Scanner;
+
 public class Runner {
 
 
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("\n");
         System.out.println("Welcome to The House Always Win");
         System.out.println("\n");
@@ -12,23 +16,53 @@ public class Runner {
 
         System.out.println("Balance: £" + fruitMachine.getCashInFromPlayer(player.getCash()));
         System.out.println("You have placed a £" + fruitMachine.getStake() + " bet, let's role the reels....");
-        while(fruitMachine.getPlayerStake()>0 ){
-        fruitMachine.enoughCashToPlayGame();
-        fruitMachine.outOfFunds();
-        fruitMachine.generateThreeWheels();
+
+            while (fruitMachine.getPlayerStake() > 0) {
+                char playAgain;
+                do {
+                    fruitMachine.enoughCashToPlayGame();
+                    fruitMachine.outOfFunds();
+                    fruitMachine.generateThreeWheels();
 //        System.out.println("---------------------------");
-        System.out.println("\n");
-        System.out.println("Reel 1    " + "Reel 2    " + "Reel 3     ");
-        System.out.println(fruitMachine.setReelOneChoice()+ " | " + fruitMachine.setReelTwoChoice() + " | " + fruitMachine.setReelThreeChoice());
-        System.out.println("\n");
-        fruitMachine.randomiseReelChoice();
-        fruitMachine.resultOfSpin();
-        System.out.println("\n");
-        System.out.println("Balance remaining: £" + fruitMachine.getPlayerStake());
-        System.out.println("Winnings to date: £" + fruitMachine.getPayout());
+                    System.out.println("\n");
+                    System.out.println("Reel 1    " + "Reel 2    " + "Reel 3     ");
+                    System.out.println(fruitMachine.setReelOneChoice() + " | " + fruitMachine.setReelTwoChoice() + " | " + fruitMachine.setReelThreeChoice());
+                    System.out.println("\n");
+                    fruitMachine.randomiseReelChoice();
+                    fruitMachine.resultOfSpin();
+                    System.out.println("\n");
+                    System.out.println("Balance remaining: £" + fruitMachine.getPlayerStake());
+                    System.out.println("Winnings to date: £" + fruitMachine.getPayout());
+                    System.out.println("\n");
+                    System.out.println("Press 'Y' to play again, or any other button to exit");
+                    String input = keyboard.next();
+                    playAgain = input.charAt(0);
+
+                }
+
+
+                while (playAgain == 'Y' || playAgain == 'y');
+                System.out.println("Thanks for playing!");
+                System.exit(0);
+
+
+            }
+
+
+
+//
+            }
+
         }
 
 //        System.out.println("Balance in machine: £" + fruitMachine.getCashIn());
 
-    }
-}
+//
+//                if (playAgain == ('Y') || playAgain == ('y')) {
+//                continue;
+//                } else if (playAgain == ('N') || playAgain == ('n')) {
+//                System.out.println("Thanks for playing!");
+//                break;
+//                } else if (playAgain != ('N') || playAgain == ('n') || playAgain != ('y') || playAgain != ('Y')) {
+//                System.out.println("Please press Y or N");
+//                continue;
