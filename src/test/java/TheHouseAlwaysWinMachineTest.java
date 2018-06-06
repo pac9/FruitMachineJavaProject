b262313383dd656585b5f18d10e37d4020a92b6a
pac9 <pac9@btinternet.com>
2018-06-06 14:16:03 +0100

    set up more tests in thehousealwayswins

--- src/test/java/TheHouseAlwaysWinMachineTest.java
+++ src/test/java/TheHouseAlwaysWinMachineTest.java
@@ -1,24 +1,92 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class TheHouseAlwaysWinMachineTest {
 
     TheHouseAlwaysWinMachine theHouseAlwaysWinMachine;
-    FruitMachine fruitMachine;
+    Player player;
+
 
     @Before
-    public void before(){
-        fruitMachine = new TheHouseAlwaysWinMachine("The House Always Wins");
+    public void before() {
+        player = new Player(10);
+        theHouseAlwaysWinMachine = new TheHouseAlwaysWinMachine("The House Always Wins");
     }
 
     @Test
-    public void hasName(){
-        assertEquals("The House Always Wins", fruitMachine.getName());
+    public void hasName() {
+        assertEquals("The House Always Wins", theHouseAlwaysWinMachine.getName());
+    }
+
+    @Test
+    public void hasCashIn() {
+        assertEquals(0, theHouseAlwaysWinMachine.getCashIn());
+    }
+
+    @Test
+    public void hasStake() {
+        assertEquals(1, theHouseAlwaysWinMachine.getStake());
+    }
+
+    @Test
+    public void hasPayout() {
+        assertEquals(0, theHouseAlwaysWinMachine.getPayout());
+    }
+
+    @Test
+    public void hasPlayerStake() {
+        assertEquals(0, theHouseAlwaysWinMachine.getPlayerStake());
     }
 
 
+    @Test
+    public void hasReel1() {
+        Enum outcome = theHouseAlwaysWinMachine.setReelOneChoice();
+        assertNotNull(outcome);
+    }
 
+    @Test
+    public void hasReel2() {
+        Enum outcome = theHouseAlwaysWinMachine.setReelTwoChoice();
+        assertNotNull(outcome);
+    }
+
+    @Test
+    public void hasReel3() {
+        Enum outcome = theHouseAlwaysWinMachine.setReelThreeChoice();
+        assertNotNull(outcome);
+    }
+
+    @Test
+    public void hasOutcomeOfSpins() {
+        theHouseAlwaysWinMachine.setReelTwoChoice();
+        theHouseAlwaysWinMachine.setReelOneChoice();
+        theHouseAlwaysWinMachine.setReelThreeChoice();
+
+        assertEquals(0, theHouseAlwaysWinMachine.resultOfSpin());
+        //this is on assumption that player loses. Need to find a better way to prove this test as
+        //currently proving this via the debugger.
+    }
+
+    @Test
+    public void canGetCashInFromPlayer(){
+        assertEquals(10, theHouseAlwaysWinMachine.getCashInFromPlayer(player.getCash()));
+    }
+
+    @Test
+    public void hasRandomReelChoice(){
+        Enum outcome = theHouseAlwaysWinMachine.randomiseReelChoice();
+        assertNotNull(outcome);
+    }
 
 }
+
+
+
+
+
